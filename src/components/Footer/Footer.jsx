f1cf510e188da9c0ba18c4ccec0dd3baca2a6929
Surya Teja <gsuryateja05@gmail.com>
2023-09-10 21:59:33 +0530

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ class Footer extends React.Component {
             <Col md="3">
               <Nav>
               <NavItem>
-              &copy; 2021 Made with <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNCIgaGVpZ2h0PSIxNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJyZWQiIGQ9Ik0xMiA0LjQxOWMtMi44MjYtNS42OTUtMTEuOTk5LTQuMDY0LTExLjk5OSAzLjI3IDAgNy4yNyA5LjkwMyAxMC45MzggMTEuOTk5IDE1LjMxMSAyLjA5Ni00LjM3MyAxMi04LjA0MSAxMi0xNS4zMTEgMC03LjMyNy05LjE3LTguOTcyLTEyLTMuMjd6Ii8+PC9zdmc+" alt="love icon" className="Footer__FooterIcon-sc-1dgef3-1 bhSTbi"/> by Surya Teja  <br/>
+              &copy; 2023 Made with <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNCIgaGVpZ2h0PSIxNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJyZWQiIGQ9Ik0xMiA0LjQxOWMtMi44MjYtNS42OTUtMTEuOTk5LTQuMDY0LTExLjk5OSAzLjI3IDAgNy4yNyA5LjkwMyAxMC45MzggMTEuOTk5IDE1LjMxMSAyLjA5Ni00LjM3MyAxMi04LjA0MSAxMi0xNS4zMTEgMC03LjMyNy05LjE3LTguOTcyLTEyLTMuMjd6Ii8+PC9zdmc+" alt="love icon" className="Footer__FooterIcon-sc-1dgef3-1 bhSTbi"/> by Surya Teja  <br/>
               Design Inspiration <a href="https://www.creative-tim.com"> Creative Tim</a> <br/>
               Built with <a href="https://reactjs.org/">React</a>       
                 </NavItem>
