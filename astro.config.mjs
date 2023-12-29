452cf469af93736291117160d01157838f5c76bb
Surya Teja G <gsuryateja05@gmail.com>
2023-12-30 00:59:09 +0530

    updated portoflio website design and migrated to astro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [icon(), tailwind(), mdx()],
+});
