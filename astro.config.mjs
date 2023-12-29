74b8df38be096864827f1af950b26c5df751a6fa
Surya Teja G <gsuryateja05@gmail.com>
2023-12-30 02:09:35 +0530

    updated deploy.yml and astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon(), tailwind(), mdx()],
+	integrations: [icon(), tailwind(), mdx()],
+	site: 'https://gsuryateja.com',
 });
